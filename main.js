2d125db2f3635c12556b9180b17da97e43ff55c6
ichigo-dev <ichigo@web-freework.com>
2021-01-31 15:05:54 +0900

    v-on:

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ var app = new Vue({
     red: 'red',
     seen: true,
     array: [0, 1, 2, 3, 4],
+    time: 'morning',
+  },
+  methods: {
+    changeTime: function(){
+      if(this.time == 'morning'){
+        this.time = 'night';
+      }else if(this.time == 'night'){
+        this.time = 'morning';
+      }
+    }
   }
 })
